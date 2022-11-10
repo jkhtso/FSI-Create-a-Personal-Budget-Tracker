d253a97866a5bab1567e36255c8a37beaa6fe697
jkhtso <jonathantso1987@gmail.com>
2022-11-09 19:49:58 -0500

    First Attempt

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+
+for(let i=0;i < weeklyExpenseQuestions.lenght;i++)
+{
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+weeklyExpenses = weeklyExpenses + answer
+}
+console.log(answer)
